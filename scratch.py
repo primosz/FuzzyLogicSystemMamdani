de4b61bb8f482ff18794b97eec4a745b1bad724c
Piotr <valadin96@gmail.com>
2020-01-09 14:57:11 +0100

    Merged 2 files
    Fuzzy algorithm is now part of fuzzy helper application inside scratch.py file

--- scratch.py
+++ scratch.py
@@ -7,10 +7,11 @@ import csv
 from datetime import datetime
 from tkinter import messagebox
 from tkinter import filedialog
-
+import matplotlib.pyplot as plt
+import os
 root = Tk()
 root.title("Fuzzy")
-root.geometry("1440x600")
+root.geometry("1440x800")
 
 
 class Set(object):
@@ -48,6 +49,12 @@ functions = ["Triangle", "Trapeze", "Gaussian"]
 clickedFunction = StringVar()
 clickedFunction.set(functions[0])
 x_values = np.linspace(-5, 40, 150)
+
+input_sets = []
+output_sets = []
+rules = []
+temp1 = 0
+temp2 = 0
 
 
 # plot functions
@@ -239,6 +246,233 @@ def addSet():
         updatePlots()
 
 
+#algorithm functions and classes
+
+class Triangle:
+    def __init__(self, name, start, peak, end, none=0):
+        self.name = name
+        self.a = start
+        self.b = peak
+        self.c = end
+
+    def __str__(self):
+        return ' ' + self.name + str(self.a) + str(self.b) + str(self.c)
+
+    def __call__(self, x):
+        if x < self.a or x > self.c:
+            return {'set': self.name, 'value': 0}
+        elif x == self.b:
+            return {'set': self.name, 'value': 1}
+        elif x < self.b:
+            return {'set': self.name, 'value': (x - self.a) / (self.b - self.a)}
+        elif x > self.b:
+            return {'set': self.name, 'value': 1 - ((x - self.b) / (self.c - self.b))}
+
+
+class Trapezoid:
+    def __init__(self, name, start, peak_first, peak_second, end):
+        self.name = name
+        self.a = start
+        self.b = peak_first
+        self.c = peak_second
+        self.d = end
+
+    def __call__(self, x):
+        if x < self.a or x > self.d:
+            return {'set': self.name, 'value': 0}
+        elif self.b <= x <= self.c:
+            return {'set': self.name, 'value': 1}
+        elif x < self.b:
+            return {'set': self.name, 'value': (x - self.a) / (self.b - self.a)}
+        elif x > self.c:
+            return {'set': self.name, 'value': 1 - ((x - self.c) / (self.d - self.c))}
+
+
+class Gaussian:
+    def __init__(self, name, expected_value, sigma, none_1=0, none_2=0):
+        self.name = name
+        self.mu = expected_value
+        self.sigma = sigma
+
+    def __call__(self, x):
+        return {'set': self.name, 'value': np.exp(-np.power(x - self.mu, 2.) / (2 * np.power(self.sigma, 2.)))}
+
+
+# you can create rules by giving parameters:
+# 1 - list of fuzze sets objects
+# 2 - list of temperatures in rule
+# 3 - what is target temperature (after 'AND')
+# 4 - what is the actions (after 'THEN')
+class Rule:
+    def __init__(self, fuzzySets, currentTable, target, then):
+        self.fuzzySets = fuzzySets
+        self.currentTable = currentTable
+        self.target = target
+        self.then = then
+
+    def __call__(self, currentTemp, targetTemp):
+        currentTempsMemberships = []
+        for fSet in self.currentTable:
+            foundSet = next((x for x in self.fuzzySets if x.name == fSet), None)
+            currentTempsMemberships.append(foundSet(currentTemp)['value'])
+
+        foundSet = next((x for x in self.fuzzySets if x.name == self.target), None)
+        targetMembership = foundSet(targetTemp)['value']
+
+        result = min(max(currentTempsMemberships), targetMembership)
+        return {'action': self.then, 'membership': result}
+
+    def __str__(self):
+        current_string = ' or '.join(self.currentTable)
+        return f"If it's {current_string} and target is {self.target} then {self.then}."
+
+
+def import_sets_from_csv(filename):
+    sets = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            tmp = [x.replace("\"", "") for x in row]
+            if tmp[1] == "Triangle":
+                sets.append(Triangle(tmp[0], int(tmp[2]), int(tmp[3]), int(tmp[4])))
+            elif tmp[1] == "Trapeze":
+                sets.append(Trapezoid(tmp[0], int(tmp[2]), int(tmp[3]), int(tmp[4]), int(tmp[5])))
+            elif tmp[1] == "Gaussian":
+                sets.append(Gaussian(tmp[0], int(tmp[2]), int(tmp[3])))
+    return sets
+
+
+def loadRules(filename, fuzzySets):
+    rules = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            currentTempslist = list(row[0].split(";"))
+            rules.append(Rule(fuzzySets, currentTempslist, row[1], row[2]))
+        return rules
+
+
+def rulesAggregation(rules, outputFuzzySets):
+    result = [0] * 200
+    for set in outputFuzzySets:
+        for i in range(200):
+            if set(i)['value'] > 0:
+                matches = [x['membership'] for x in rules if x['action'] == set.name and x['membership'] > 0]
+                if len(matches) > 0:
+                    tmp = set(i)['value']
+                    if result[i] < min(max(matches), tmp):
+                     result[i] = min(max(matches), tmp)
+    return result
+
+#defuzzification methods
+def calulcateCentroid(plot):
+    n = len(plot)
+    x = list(range(n))
+    num = 0
+    denum = 0
+    for i in range(n):
+        num += x[i] * plot[i]
+        denum += plot[i]
+    return {'name': 'Centroid', 'value': num / denum}
+
+
+
+def maxMembershipPrinciple(plot):
+    n = len(plot)
+    maxMem = 0
+    x = list(range(n))
+    for i in range(n):
+        if plot[i] > maxMem:
+            maxMem = plot[i]
+            result = x[i]
+    return {'name': 'Max Membership Principle', 'value': result}
+
+
+
+def drawFinalPlot(outputFuzzySets, aggregatedRules, points):
+
+    plt.figure(figsize=(12,6))
+    plt.ylim(0, 1)
+    for x in outputFuzzySets:
+        plt.plot(np.linspace(0, 200, 200), [x(i)['value'] for i in range(0, 200)], '--', label=x.name, linewidth='0.5')
+
+    plt.fill_between(np.linspace(0, 200, 200), aggregatedRules)
+    plt.plot(np.linspace(0, 200, 200), aggregatedRules, label="Output membership", fillstyle='full')
+    for point in points:
+        plt.axvline(x=point['value'], label=f'{point["name"]}: {point["value"]}', color='black')
+
+    plt.ylabel('memberships')
+    plt.legend()
+    plt.show()
+
+
+def importInputSets():
+    global input_sets
+    root.filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                               title="Select .CSV file",
+                                               filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+    input_sets = import_sets_from_csv(root.filename)
+    messagebox.showinfo("Import successful", "Imported file: " + root.filename +
+                        "\nNumber of imported sets: " + str(len(input_sets)))
+
+
+def importOutputSets():
+    global output_sets
+    root.filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                               title="Select .CSV file",
+                                               filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+    output_sets = import_sets_from_csv(root.filename)
+    messagebox.showinfo("Import successful", "Imported file: " + root.filename +
+                        "\nNumber of imported sets: " + str(len(output_sets)))
+
+
+def importRulesFromCSV():
+    global rules
+    root.filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                               title="Select .CSV file",
+                                               filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+    rules = loadRules(root.filename, input_sets)
+    messagebox.showinfo("Import successful", "Imported file: " + root.filename +
+                        "\nNumber of imported rules: " + str(len(rules)))
+
+
+def run():
+    if currentTemp.get() != '' and targetTemp.get() != "" and len(input_sets) > 0 and len(output_sets) > 0 and len(rules) > 0:
+        global temp1, temp2
+        temp1 = int(currentTemp.get())
+        temp2 = int(targetTemp.get())
+        print(temp1, temp2)
+
+        current = [x(temp1) for x in input_sets]
+        target = [x(temp2) for x in input_sets]
+
+        print(current)
+        print(target)
+
+        evaluatedRules = [x(temp1, temp2) for x in rules]
+        for x in evaluatedRules:
+            print(x)
+
+        aggregatedRules = rulesAggregation(evaluatedRules, output_sets)
+        centroid = calulcateCentroid(aggregatedRules)
+        maxMembershipPrincipleResult = maxMembershipPrinciple(aggregatedRules)
+
+        drawFinalPlot(output_sets, aggregatedRules, [centroid, maxMembershipPrincipleResult])
+    else:
+        messagebox.showerror("Error", "You did not provided valid data!")
+
+
+
+def printS():
+    for x in input_sets:
+       print(x)
+
+    for x in output_sets:
+        print(x)
+
+    for x in rules:
+        print(x)
+
 # dropDowns
 setsNames = list(map(lambda x: x.data['name'], setsObjects))
 
@@ -327,5 +561,33 @@ canvas.draw()
 canvas.get_tk_widget().grid(row=0, column=5, rowspan=15, columnspan=5)
 pickSet(setsObjects[1].data['name'])
 pickFun(functions[0])
+
+#algorithm frame
+algorithm_frame = Frame(root,relief='flat', borderwidth=30)
+algorithm_frame.grid(row=18, column=0, columnspan=10)
+algorithm_frame.config( pady=30, padx=20)
+
+currentTempLabel = Label(algorithm_frame, text="Current Temperature")
+currentTempLabel.grid(row=0, column=0, columnspan=2)
+currentTemp = Entry(algorithm_frame)
+currentTemp.grid(row=1, column=0, columnspan=2)
+
+targetTempLabel = Label(algorithm_frame, text="Target Temperature")
+targetTempLabel.grid(row=0, column=2, columnspan=2)
+targetTemp = Entry(algorithm_frame)
+targetTemp.grid(row=1, column=2, columnspan=2)
+
+btnImportInput = Button(algorithm_frame, command=importInputSets, text="Pick input fuzzy sets file")
+btnImportInput.grid(row=1, column=4, padx=5)
+
+btnImportOutput = Button(algorithm_frame, command=importOutputSets, text="Pick output fuzzy sets file")
+btnImportOutput.grid(row=1, column=5, padx=5)
+
+btnImportRules = Button(algorithm_frame, command=importRulesFromCSV, text="Pick rules file")
+btnImportRules.grid(row=1, column=6, padx=5)
+
+btnRun = Button(algorithm_frame, command=run, text="Run Algorithm", bg='red')
+btnRun.grid(row=1, column=8, padx=5)
+
 updatePlots()
 root.mainloop()
